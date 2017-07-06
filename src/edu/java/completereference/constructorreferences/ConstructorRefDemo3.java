04e9578e8e54b42bfef44c464bfef79fedfb08d1
Ritesh Bangal <rbangal@sapient.com>
2017-07-06 16:01:44 +0530

    constructor reference

--- src/edu/java/completereference/constructorreferences/ConstructorRefDemo3.java
+++ src/edu/java/completereference/constructorreferences/ConstructorRefDemo3.java
@@ -1,49 +1,88 @@
 package edu.java.completereference.constructorreferences;
 
 /**
- * Demonstrate a constructor reference with a generic class.
+ * Implement a simple class factory using a constructor reference.
  * 
  * @author - Ritesh
  * @version 1.0
  * @since <07-July-2017>
  */
-class ConstructorRefDemo2 {
+class ConstructorRefDemo3 {
 
-	public static void main(String[] args) {
+	/*
+	 * A factory method for class objects. The class must have a constructor that takes one parameter of type T.
+	 * R specifies the type of object being created.
+	 */
+	static <R, T> R myClassFactory(MyFunc3<R, T> cons, T v) {
+		return cons.func(v);
+	}
 
-		// Create a reference to the MyClass<T> constructor.
-		MyFuncGeneric<Integer> myClassCons = MyClassForConstructorRef<Integer>::new;
+	public static void main(String args[]) {
+		/*
+		 * Create a reference to a MyClass constructor.
+		 * In this case, new refers to the constructor that takes an argument.
+		 */
+		MyFunc3<MyClass3<Double>, Double> myClassCons = MyClass3<Double>::new;
 		
-		// Create an instance of MyClass<T> via that constructor reference.
-		MyClassForConstructorRef<Integer> mc = myClassCons.func(100);
+		// Create an instance of MyClass by use of the factory method.
+		MyClass3<Double> mc = myClassFactory(myClassCons, 100.1);
 		
-		// Use the instance of MyClass<T> just created.
+		// Use the instance of MyClass just created.
 		System.out.println("val in mc is " + mc.getVal());
+		
+		// Now, create a different class by use of myClassFactory().
+		MyFunc3<MyClass2, String> myClassCons2 = MyClass2::new;
+		
+		// Create an instance of MyClass2 by use of the factory method.
+		MyClass2 mc2 = myClassFactory(myClassCons2, "Lambda");
+		
+		// Use the instance of MyClass just created.
+		System.out.println("str in mc2 is " + mc2.getVal());
 	}
 }
 
-/*
- * MyFunc is now a generic functional interface.
- */
-interface MyFuncGeneric<T> {
-	MyClassForConstructorRef<T> func(T n);
+interface MyFunc3<R, T> {
+	R func(T n);
 }
 
-class MyClassForConstructorRef<T> {
+// A simple generic class.
+class MyClass3<T> {
 	private T val;
 
 	// A constructor that takes an argument.
-	MyClassForConstructorRef(T v) {
+	MyClass3(T v) {
 		val = v;
 	}
 
-	// This is the default constructor.
-	MyClassForConstructorRef() {
+	// The default constructor. This constructor
+	// is NOT used by this program.
+	MyClass3() {
 		val = null;
 	}
 
 	// ...
 	T getVal() {
 		return val;
+	};
+}
+
+// A simple, non-generic class.
+class MyClass2 {
+	String str;
+
+	// A constructor that takes an argument.
+	MyClass2(String s) {
+		str = s;
 	}
+
+	// The default constructor. This
+	// constructor is NOT used by this program.
+	MyClass2() {
+		str = "";
+	}
+
+	// ...
+	String getVal() {
+		return str;
+	};
 }
